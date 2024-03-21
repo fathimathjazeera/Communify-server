ed50454d998a39b80d013494bc90f201d3afeebd
fathimathjazeera <jezijazeera@gmail.com>
2024-03-21 15:36:49 +0530

    modified routes

--- routes/userRoute/user.js
+++ routes/userRoute/user.js
@@ -1,49 +1,37 @@
-const express=require('express')
-const userRoute=express.Router()
-const userController= require('../../controllers/userController/user')
-const authentication=require('../../middlewares/jwt')
-const tryCatch=require('../../middlewares/tryCatchMiddleware')
+const express = require("express");
+const userRoute = express.Router();
+const {
+  register,
+  login,
+  verifyEmail,
+  refreshToken,
+  userProfile,
+  viewUserPost,
+  viewUserComment,
+  uploadAvatar,
+  editProfile,
+  deleteProfile,
+  userUpvoted,
+  userDownvoted,
+} = require("../../controllers/userController/user");
+const authentication = require("../../middlewares/jwt");
+const tryCatch = require("../../middlewares/tryCatchMiddleware");
 
 
 
+userRoute.post("/register", tryCatch(register));
+userRoute.post("/login", tryCatch(login));
+userRoute.post("/token", tryCatch(refreshToken));
+userRoute.get("/users/userprofile", authentication, tryCatch(userProfile));
+userRoute.get("/users/viewuserpost", authentication, tryCatch(viewUserPost));
+userRoute.get("/users/viewusercomment",authentication,tryCatch(viewUserComment));
+userRoute.put("/users/uploadavatar", authentication, tryCatch(uploadAvatar));
+userRoute.put("/users/editprofile", authentication, tryCatch(editProfile));
+userRoute.delete("/users/deleteprofile",authentication,tryCatch(deleteProfile));
+userRoute.get("/users/userupvote", authentication, tryCatch(userUpvoted));
+userRoute.get("/users/userdownvote", authentication, tryCatch(userDownvoted));
+userRoute.post("/verify-email",verifyEmail);
+
+module.exports = userRoute;
 
 
-
-userRoute.post('/register',tryCatch(userController.register))
-userRoute.post('/login',tryCatch(userController.login))
-userRoute.post('/token',tryCatch(userController.refreshToken))
-userRoute.get('/users/userprofile',authentication,tryCatch(userController.userProfile))
-userRoute.get('/users/viewuserpost',authentication,tryCatch(userController.viewUserPost))
-userRoute.get('/users/viewusercomment',authentication,tryCatch(userController.viewUserComment))
-userRoute.put('/users/uploadavatar',authentication,tryCatch(userController.uploadAvatar))
-userRoute.put('/users/editprofile',authentication,tryCatch(userController.editProfile))
-userRoute.delete('/users/deleteprofile',authentication,tryCatch(userController.deleteProfile))
-userRoute.get('/users/userupvote',authentication,tryCatch(userController.userUpvoted))
-userRoute.get('/users/userdownvote',authentication, tryCatch(userController.userDownvoted))
-
-
-
-
-
-
-
-module.exports=userRoute
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports=userRoute
